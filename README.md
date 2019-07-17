fddd62b0807ea272a000a5252d1bc2959324fc07
Joseph Martinez <josephsmartinez@gmail.com>
2019-07-16 21:01:24 -0700

    updated api routes and added deploy information

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PostgreSQLgo
+
 PostgreSQL with Golang
+
+- Build all internal search functions first
+- Take args from the command prompt to query the database.
+
+Start app
+> sh postgres-setup.sh
+> go run app/app.go
+
+Queries
+> curl -i localhost:8080/employees
+> curl -i localhost:8080/employees/find?firstname=Rich
--- app/app.go
+++ app/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
@@ -17,44 +18,92 @@ type Employee struct {
 	favoriteColor string
 }
 
-func main() {
+var db *sql.DB
 
+func init() {
 	// Example conntection string
 	// postgres://[USERNAME]:[PASSWORD]@[HOSTNAME]/[DATABASE]?sslmode=disable
-	db, err := sql.Open("postgres", "postgres://postgres:pass123@localhost/sampledata?sslmode=disable")
+	var err error
+	db, err = sql.Open("postgres", "postgres://postgres:pass123@localhost/sampledata?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("You are connected to your database.")
+}
 
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/employees", employeeIndex)
+	http.HandleFunc("/employees/find", getEmployeeByName)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "index is up!\nMETHOD="+r.Method)
+}
+
+func employeeIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
 	rows, err := db.Query("SELECT * FROM employees;")
 	if err != nil {
-		panic(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
 	}
 	defer rows.Close()
 
 	employees := make([]Employee, 0)
 	for rows.Next() {
 		employee := Employee{}
-		//
 		err := rows.Scan(&employee.id, &employee.firstName, &employee.lastName, &employee.email, &employee.gender, &employee.favoriteColor) // order matters
 		if err != nil {
-			panic(err)
+			http.Error(w, http.StatusText(500), 500)
+			return
 		}
 		employees = append(employees, employee)
 	}
 	if err = rows.Err(); err != nil {
-		panic(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
 	}
 
 	for _, emp := range employees {
 		// fmt.Println(bk.isbn, bk.title, bk.author, bk.price)
-		fmt.Printf("%v, %s, %s, %s, %s, %s\n", emp.id, emp.firstName, emp.lastName, emp.email, emp.gender, emp.favoriteColor)
+		fmt.Fprintf(w, "%v, %s, %s, %s, %s, %s\n", emp.id, emp.firstName, emp.lastName, emp.email, emp.gender, emp.favoriteColor)
 	}
+
+}
+
+func getEmployeeByName(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fname := r.FormValue("firstname")
+	if fname == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM employees WHERE first_name = $1", fname)
+
+	employee := Employee{}
+	err := row.Scan(&employee.id, &employee.firstName, &employee.lastName, &employee.email, &employee.gender, &employee.favoriteColor)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "%v, %s, %s, %s, %s, %s\n", employee.id, employee.firstName, employee.lastName, employee.email, employee.gender, employee.favoriteColor)
 }
